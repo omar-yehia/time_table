e66a5c07d76d3d8296b9ed23c3e378c424bfb469
omar_yahia <omaryk77@gmail.com>
2022-03-30 23:50:32 +0200

    initialize migrations and controllers

---
+++ database/migrations/2022_03_30_214611_create_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('pharmacy_id');
+            $table->date('date');
+            $table->string('day');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
